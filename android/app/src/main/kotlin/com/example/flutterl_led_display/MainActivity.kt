723cc618b549437e8caa8917e6c543098f2de703
Vandatgsts <datnv2k2@gmail.com>
2023-12-29 09:06:28 +0700

    1

---
+++ android/app/src/main/kotlin/com/example/flutterl_led_display/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterl_led_display
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/text_to_picture_converter.dart
@@ -0,0 +1,56 @@
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+
+class TextToPictureConverter {
+  static ui.Picture convert({
+    @required String text,
+    @required double canvasSize,
+    @required bool border,
+  }) {
+    final recorder = ui.PictureRecorder();
+    final canvas = Canvas(
+      recorder,
+      Rect.fromPoints(
+        Offset(0.0, 0.0),
+        Offset(canvasSize, canvasSize),
+      ),
+    );
+
+    final Color color = Colors.white;
+
+    if (border) {
+      final stroke = Paint()
+        ..color = color
+        ..style = PaintingStyle.stroke;
+
+      canvas.drawRect(Rect.fromLTWH(0.0, 0.0, canvasSize, canvasSize), stroke);
+    }
+
+    final textStyle = TextStyle(
+      fontFamily: "Monospace",
+      color: color,
+      fontSize: 24,
+    );
+
+    final textSpan = TextSpan(style: textStyle, text: text);
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );
+
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: double.infinity,
+    );
+
+    final offset = Offset(
+      (canvasSize - textPainter.width) * 0.5,
+      (canvasSize - textPainter.height) * 0.5,
+    );
+
+    textPainter.paint(canvas, offset);
+
+    return recorder.endRecording();
+  }
+}
